cb7c9d4b8427d86ff7bce17f247035383bac1977
Chanadu <chandu.peddada@gmail.com>
2024-05-16 18:12:09 -0500

    colors fix

--- src/routes/ColorModeSwitch.svelte
+++ src/routes/ColorModeSwitch.svelte
@@ -11,18 +11,18 @@
 			bind:checked="{$darkState}"
 		/>
 		<img
-			src="images/light-mode.svg"
+			src="{$darkState ? 'images/light-mode-dark.svg' : 'images/light-mode-light.svg'}"
 			alt="Light Mode"
-			class="m-4 h-8 w-8 bg-primary"
+			class="m-4 h-8 w-8"
 		/>
 		<!-- <span class=" me-3 text-sm font-medium">Light Mode</span> -->
 		<div
 			class="peer relative h-6 w-11 rounded-full bg-primary after:absolute after:start-[2px] after:top-[2px] after:h-5 after:w-5 after:rounded-full after:border after:border-accent after:bg-text after:transition-all after:content-[''] peer-checked:bg-secondary peer-checked:after:translate-x-full peer-checked:after:border-text peer-focus:outline-none peer-focus:ring-4 rtl:peer-checked:after:-translate-x-full"
 		></div>
 		<img
-			src="images/dark-mode.svg"
+			src="{$darkState ? 'images/dark-mode-dark.svg' : 'images/dark-mode-light.svg'}"
 			alt="Dark Mode"
-			class="m-4 h-8 w-8 bg-secondary"
+			class="m-4 h-10 w-10"
 		/>
 	</label>
 </div>
